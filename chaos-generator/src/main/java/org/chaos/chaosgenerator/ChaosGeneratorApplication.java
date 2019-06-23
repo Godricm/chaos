7c7e267a593923de5e2917cbef073e76368354fe
Godric <miaochengod@outlook.com>
2019-06-23 22:39:08 +0800

    [add] 创建代码生成，参照SpringBootCodeGenerator

---
+++ chaos-generator/src/main/java/org/chaos/chaosgenerator/ChaosGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.chaos.chaosgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaosGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaosGeneratorApplication.class, args);
+    }
+
+}
